177c53cdd88434305281269ebf0358d549c804d5
PalindroneV2 <asfadasfag@gmail.com>
2022-02-23 21:31:51 -0700

    fixed colt scope alignment

--- lua/arc9/common/attachments/bo1_optic_colt.lua
+++ lua/arc9/common/attachments/bo1_optic_colt.lua
@@ -24,7 +24,7 @@ ATT.ModelOffset = Vector(0, 0, 0)
 
 ATT.Sights = {
     {
-        Pos = Vector(-0, 6.5, -1.05),
+        Pos = Vector(-0, 6.5, -1.0475),
         Ang = Angle(0, 0, 0),
         Magnification = 1,
         IgnoreExtra = true
