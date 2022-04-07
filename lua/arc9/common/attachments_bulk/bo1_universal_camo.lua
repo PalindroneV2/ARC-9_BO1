73ce892f07be2d85fa797fed4640dbbba44f34aa
PalindroneV2 <asfadasfag@gmail.com>
2022-04-07 01:22:02 -0700

    camo shit

--- lua/arc9/common/attachments_bulk/bo1_universal_camo.lua
+++ lua/arc9/common/attachments_bulk/bo1_universal_camo.lua
@@ -1,11 +1,215 @@
 local ATT = {}
 
-ATT.PrintName = "Red"
-ATT.CompactName = "RED"
-ATT.Icon = Material("models/weapons/arc9/bo1/red")
+ATT.PrintName = "Bakelite"
+ATT.CompactName = "BAKE"
+ATT.Icon = Material("models/weapons/arc9/bo1/camos/bakelite.png")
 ATT.Category = "universal_camo"
 ATT.Description = [[Universal red color.]]
 ATT.MenuCategory = "ARC-9 - Camos"
-ATT.CustomCamoTexture = "models/weapons/arc9/bo1/red"
+ATT.CustomCamoTexture = "models/weapons/arc9/bo1/camos/bakelite"
 
-ARC9.LoadAttachment(ATT, "camo_red")
+ARC9.LoadAttachment(ATT, "camo_bakelite")
+
+ATT = {}
+
+ATT.PrintName = "Dark Bakelite"
+ATT.CompactName = "DARKBAKE"
+ATT.Icon = Material("models/weapons/arc9/bo1/camos/darkbake.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[Universal red color.]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/bo1/camos/darkbake"
+
+ARC9.LoadAttachment(ATT, "camo_darkbake")
+
+ATT = {}
+
+ATT.PrintName = "Digital Tan"
+ATT.CompactName = "DIGITAN"
+ATT.Icon = Material("models/weapons/arc9/bo1/camos/digital_tan.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[Universal red color.]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/bo1/camos/digital_tan"
+
+ARC9.LoadAttachment(ATT, "camo_digital_tan")
+
+ATT = {}
+
+ATT.PrintName = "Nevada Desert"
+ATT.CompactName = "NEVADA"
+ATT.Icon = Material("models/weapons/arc9/bo1/camos/desert_nevada.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[Universal red color.]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/bo1/camos/desert_nevada"
+
+ARC9.LoadAttachment(ATT, "camo_nevada")
+
+ATT = {}
+
+ATT.PrintName = "Russian Desert"
+ATT.CompactName = "DESERT RUS"
+ATT.Icon = Material("models/weapons/arc9/bo1/camos/desert_rus.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[Universal red color.]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/bo1/camos/desert_rus"
+
+ARC9.LoadAttachment(ATT, "camo_desert_rus")
+
+ATT = {}
+
+ATT.PrintName = "Saharan Desert"
+ATT.CompactName = "SAHARA"
+ATT.Icon = Material("models/weapons/arc9/bo1/camos/desert_sahara.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[Universal red color.]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/bo1/camos/desert_sahara"
+
+ARC9.LoadAttachment(ATT, "camo_sahara")
+
+ATT = {}
+
+ATT.PrintName = "Woodland"
+ATT.CompactName = "WOODLAND"
+ATT.Icon = Material("models/weapons/arc9/bo1/camos/woodland.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[Universal red color.]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/bo1/camos/woodland"
+
+ARC9.LoadAttachment(ATT, "camo_woodland")
+
+ATT = {}
+
+ATT.PrintName = "Woodland Flora"
+ATT.CompactName = "FLORA"
+ATT.Icon = Material("models/weapons/arc9/bo1/camos/woodland_flora.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[Universal red color.]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/bo1/camos/woodland_flora"
+
+ARC9.LoadAttachment(ATT, "camo_flora")
+
+ATT = {}
+
+ATT.PrintName = "Flecktarn"
+ATT.CompactName = "FLECK"
+ATT.Icon = Material("models/weapons/arc9/bo1/camos/german.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[Universal red color.]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/bo1/camos/german"
+
+ARC9.LoadAttachment(ATT, "camo_flecktarn")
+
+ATT = {}
+
+ATT.PrintName = "Winter Siberia"
+ATT.CompactName = "SIBERIA"
+ATT.Icon = Material("models/weapons/arc9/bo1/camos/winter_siberia.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[Universal red color.]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/bo1/camos/winter_siberia"
+
+ARC9.LoadAttachment(ATT, "camo_siberia")
+
+ATT = {}
+
+ATT.PrintName = "Winter Yukon"
+ATT.CompactName = "YUKON"
+ATT.Icon = Material("models/weapons/arc9/bo1/camos/winter_yukon.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[Universal red color.]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/bo1/camos/winter_yukon"
+
+ARC9.LoadAttachment(ATT, "camo_yukon")
+
+ATT = {}
+
+ATT.PrintName = "American Jungle"
+ATT.CompactName = "JUNGUS"
+ATT.Icon = Material("models/weapons/arc9/bo1/camos/jungle_us.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[Universal red color.]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/bo1/camos/jungle_us"
+
+ARC9.LoadAttachment(ATT, "camo_jungle_us")
+
+ATT = {}
+
+ATT.PrintName = "Russian Jungle"
+ATT.CompactName = "JUNGRUS"
+ATT.Icon = Material("models/weapons/arc9/bo1/camos/jungle_rus.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[Universal red color.]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/bo1/camos/jungle_rus"
+
+ARC9.LoadAttachment(ATT, "camo_jungle_rus")
+
+ATT = {}
+
+ATT.PrintName = "Tigerstripe"
+ATT.CompactName = "TIGER"
+ATT.Icon = Material("models/weapons/arc9/bo1/camos/jungle_tiger.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[Universal red color.]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/bo1/camos/jungle_tiger"
+
+ARC9.LoadAttachment(ATT, "camo_tiger")
+
+ATT = {}
+
+ATT.PrintName = "Urban Warsaw"
+ATT.CompactName = "URBAN WAR"
+ATT.Icon = Material("models/weapons/arc9/bo1/camos/urban_warsaw.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[Universal red color.]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/bo1/camos/urban_warsaw"
+
+ARC9.LoadAttachment(ATT, "camo_urban_warsaw")
+
+ATT = {}
+
+ATT.PrintName = "Urban German"
+ATT.CompactName = "URBAN GER"
+ATT.Icon = Material("models/weapons/arc9/bo1/camos/urban_german.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[Universal red color.]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/bo1/camos/urban_german"
+
+ARC9.LoadAttachment(ATT, "camo_urban_german")
+
+ATT = {}
+
+ATT.PrintName = "Urban Russian"
+ATT.CompactName = "URBAN RUS"
+ATT.Icon = Material("models/weapons/arc9/bo1/camos/urban_rus.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[Universal red color.]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/bo1/camos/urban_rus"
+
+ARC9.LoadAttachment(ATT, "camo_urban_rus")
+
+ATT = {}
+
+ATT.PrintName = "Xx_MLGMaster420_xX"
+ATT.CompactName = "MLG"
+ATT.Icon = Material("models/weapons/arc9/bo1/camos/weed_detail.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[MOM GET THE CAMERA!.]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/bo1/camos/weed_detail"
+
+ARC9.LoadAttachment(ATT, "camo_weed")
