67a9d7829b06a7afa1291dc8bd6bb18d224c7e45
PalindroneV2 <asfadasfag@gmail.com>
2022-07-18 17:50:45 -0700

    Create bo2_tac_mtar.lua

---
+++ lua/arc9/common/attachments/bo2_tac_mtar.lua
@@ -0,0 +1,31 @@
+ATT.PrintName = "MTAR Laser Pointer"
+ATT.CompactName = [[MTAR]]
+ATT.Icon = Material("entities/bo1_atts/tactical/bo2_laser.png", "mips smooth")
+ATT.Description = [[
+    Tacical laser pointer. Tighter aim when firing from hip, less dispersion when moving.
+
+    Belongs to Black Ops II.
+]]
+ATT.Pros = {
+    "+ Clearer sight picture"
+}
+ATT.Cons = {}
+ATT.SortOrder = 1
+ATT.MenuCategory = "ARC-9 - MWC Attachments"
+ATT.Free = false
+-- ATT.Folder = "RDS"
+
+ATT.Category = {"bo2_mtar_tactical"}
+
+ATT.Model = "models/weapons/arc9/atts/bo2_mtar_laser.mdl"
+ATT.Scale = 1
+ATT.ModelOffset = Vector(-6, -1.2, -1.4)
+ATT.ModelAngleOffset = Angle(0, 0, 110)
+
+ATT.Laser = true
+ATT.LaserStrength = 3
+ATT.LaserColor = Color(0, 255, 136)
+ATT.LaserAttachment = 1
+
+ATT.SpreadMultHipFire = 0.8
+--ATT.SpreadMultMove = 0.8
