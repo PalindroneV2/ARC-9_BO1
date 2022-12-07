b3250097ff9dee3fdecfd3dcd0c0a3f4797481a9
TheOnly8Z <888888zombies@gmail.com>
2022-12-07 01:44:40 -0600

    balance china lake

--- lua/weapons/arc9_bo1_chinalake.lua
+++ lua/weapons/arc9_bo1_chinalake.lua
@@ -39,9 +39,9 @@ SWEP.ViewModelFOVBase = 75
 
 SWEP.DefaultBodygroups = "00000000000000"
 
-SWEP.DamageMax = 25
-SWEP.DamageMin = 15 -- damage done at maximum range
-SWEP.RangeMax = 6000
+SWEP.DamageMax = 250
+SWEP.DamageMin = 250
+SWEP.RangeMax = 1000
 SWEP.RangeMin = 1000
 SWEP.Penetration = 0
 SWEP.DamageType = nil
@@ -83,14 +83,14 @@ SWEP.RecoilAutoControl = 0.5
 SWEP.RecoilKick = 0
 SWEP.PushBackForce = 1
 
-SWEP.Spread = 0
+SWEP.Spread = 0.008
 SWEP.SpreadAddRecoil = 0
 
-SWEP.SpreadMultSights = 0.1
-SWEP.SpreadAddHipFire = 0.03
+SWEP.SpreadMultSights = 1
+SWEP.SpreadAddHipFire = 0.06
 --SWEP.SpreadAddMove = 0
-SWEP.SpreadAddMidAir = 0
--- SWEP.SpreadAddShooting = math.rad(5 / 37.5) -- 0
+SWEP.SpreadAddMidAir = 0.1
+-- SWEP.SpreadAddShooting = math.rad(5 / 37.5) -- 0 -- = 0
 
 SWEP.RecoilPatternDrift = 20
 
