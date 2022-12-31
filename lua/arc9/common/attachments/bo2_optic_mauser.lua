a1346e542fd2ba2e861a908e0f50cac292d35223
PalindroneV2 <asfadasfag@gmail.com>
2022-12-31 14:13:06 -0700

    BO2 Mauser replaces BO3 mauser
    Someone help me finish this one

---
+++ lua/arc9/common/attachments/bo2_optic_mauser.lua
@@ -0,0 +1,44 @@
+ATT.PrintName = "Mauser Carbine Scope"
+ATT.CompactName = [[C.SCOPE]]
+ATT.Icon = Material("entities/bo1_atts/optics/waw_telescopic.png", "mips smooth")
+ATT.Description = [[
+    Medium range combat scope for improved precision at longer ranges.
+
+    Belongs to Black Ops II.
+]]
+ATT.Pros = {
+    "+ 3x Zoom",
+    "+ Clearer sight picture"
+}
+ATT.Cons = {}
+ATT.SortOrder = 4
+ATT.MenuCategory = "ARC-9 - BO1 Attachments"
+ATT.Free = false
+ATT.Folder = "SCOPE"
+
+ATT.Category = {"bo2_optic_mauser"}
+
+ATT.Model = "models/weapons/arc9/atts/bo2_mauser_scope.mdl"
+ATT.Scale = 1
+ATT.ModelOffset = Vector(0, 0, 0)
+
+ATT.Sights = {
+    {
+        Pos = Vector(0, 5, -1.1525),
+        Ang = Angle(0, 0, 0),
+        ViewModelFOV = 60,
+        Magnification = 1,
+        IgnoreExtra = true
+    },
+}
+
+ATT.RTScope = true
+ATT.RTScopeSubmatIndex = 2
+ATT.RTScopeFOV = 9
+ATT.RTScopeRes = 512
+ATT.RTScopeSurface = Material("models/weapons/arc9/bo2/bo2_acogs/acog_lens")
+ATT.RTScopeReticle = Material("hud/arc9_bo1/scopes/bo3_mauserscope.png", "mips smooth")
+ATT.RTScopeReticleScale = 0.7
+ATT.RTScopeShadowIntensity = 1.5
+ATT.RTScopeNoPP = false
+ATT.RTScopeColorable = true
