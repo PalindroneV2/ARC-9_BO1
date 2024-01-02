707534c283fb1dcb985c25845de432a42ea5ad6c
PalindroneV2 <asfadasfag@gmail.com>
2024-01-02 13:16:14 -0700

    aaaa

---
+++ lua/arc9/shared/sh_arc9_boc_icons.lua
@@ -0,0 +1,3 @@
+list.Set("ContentCategoryIcons", "ARC9 - Black Ops", "entities/bo1_generic.png")
+list.Set("ContentCategoryIcons", "ARC9 - Black Ops II", "entities/bo2_generic.png")
+list.Set("ContentCategoryIcons", "ARC9 - World at War", "entities/waw_generic.png")
--- lua/arc9/shared/sh_arc9_cod_extras.lua
+++
@@ -1,5 +0,0 @@
---I gotta figure out how to make a convar
-
---[[if SERVER then
-	CreateConVar("arc9_codextras_merge", 0, FCVAR_REPLICATED, "Merge COD Extras Category with Black Ops.", 0, 1)
-end]]
